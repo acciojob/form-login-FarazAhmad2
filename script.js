06ffb21a7405a3ca07dd64a175dde79df70908de
Faraz Ahmad <102180103+FarazAhmad2@users.noreply.github.com>
2024-03-07 18:06:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
+	var formData = new FormData(document.getElementById('form1'));
+	var formValues = '';
+  for (var pair of formData.entries()) {
+    formValues += pair[1].trim() + ' ';
+  }
+  alert(formValues.trim());
 
 }
